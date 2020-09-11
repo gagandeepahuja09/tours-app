7757741812e115566cb5fe949e5eaf66e3a2b6ed
gagandeep.ahuja <gagandeepahuja09@gmail.com>
2020-09-11 19:38:21 +0530

    intro to mongo, creating collns, documents

--- Logics.js
+++ Logics.js
@@ -52,4 +52,35 @@ Success / Fail(Client) / Error(Server)
 	* next() helps to move to next element in the middleware stack.
 	* Use-case: When we want a combination of similar routes to go through a middleware. eg. a validator to 
 	check if the given id is valid.
+
+#65 Chaining Multiple Middlewares
+	* We do this, so that the route handler can focus on its task and not on common tasks like validation & 
+	authentication, etc.
+#70 MongoDB Intro
+	Database => Table -> Collections
+				Rows  -> Documents
+	Features Of Mongo:
+		* It is document based => feild, value pair data structures,
+			{
+
+			}
+		* Scalable: Very easy to distribute data across multiple machines as our users and amount of data grows.
+		* Flexible: No document data schema, so each document can have different number and types of fields.
+		* Performant: Embedded data models, sharding, indexing, flexible documents, native duplication, etc.
+	
+	* Document structure used: BSON(Like JSON but typed)
+	* Fields are equivalent to column
+	* Another advantage of Mongo DB: We can easily use array or object as our value, which is not directly 
+	possible in SQL, so we need to find workarounds => generally using joins.
+	* We can also use embedded documents in mongo. Example: embedding comments document in posts document.
+
+#73 Creating A Local Database
+	* use natours-test => Create a new db and move to it(Only move if already exists)
+	* show collections
+	* db.tours.insertOne() => Will create a tours collection if does not already exist.
+	* db.tours.insertOne({ name: "The Forest Hiker", price: 297, rating: 4.7 }) => Creates a collection and 
+	inserts a document.
+
+#74 CRUD: Creating Documents
+	* db.tours.insertMany([{}, {}])
 */
