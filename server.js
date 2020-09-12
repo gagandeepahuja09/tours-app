6dac7aa46bf2955c8417521660dac9c6f672fa05
gagandeep.ahuja <gagandeepahuja09@gmail.com>
2020-09-12 17:22:29 +0530

    creating documents and testing the model

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ mongoose.connect(DB, {
 const tourSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [ true, 'A tour must have a name']
+        required: [ true, 'A tour must have a name'],
+        unique: true
     }, 
     rating: {
         type: Number,
@@ -28,6 +29,21 @@ const tourSchema = new mongoose.Schema({
 })
 
 const Tour = mongoose.model('Tour', tourSchema)
+
+const testTour = new Tour({
+    name: "The Forest Hiker",
+    rating: 4.7,
+    price: 497
+})
+
+testTour
+    .save()
+    .then(doc => {
+        console.log(doc)
+    })
+    .catch(err => {
+        console.log('ERROR :', err)
+    })
 
 const port = process.env.PORT || 4199
 app.listen(port, () => {
