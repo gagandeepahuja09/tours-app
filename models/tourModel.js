9812b2b17a1d6a8f5d6258f4ece8632f9b0a819c
gagandeep.ahuja <gagandeepahuja09@gmail.com>
2020-09-13 11:03:40 +0530

    modelling the tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -3,17 +3,54 @@ const mongoose = require('mongoose')
 const tourSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [ true, 'A tour must have a name'],
+        required: [true, 'A tour must have a name'],
         unique: true
     }, 
     rating: {
         type: Number,
         default: 3.5
     },
+    duration: {
+        type: Number,
+        required: [true, 'A tour must have a group size']
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size']
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a difficulty']
+    },
+    // We will calculate these two and are not expected a input, hence not required
+    ratingsAverage: {
+        type: Number,
+        default: 3.5
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
+    },
     price: {
         type: Number,
-        required: [ true, 'A tour must have a price']
-    }
+        required: [true, 'A tour must have a price']
+    },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a summary']
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    startDates: [Date]
 })
 
 const Tour = mongoose.model('Tour', tourSchema)
